5d521aeb412c98dd99d3cd3ec0cb214e117afd11
KeenGWatanabe <mckeenweb@gmail.com>
2025-02-12 21:39:08 +0800

    r

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "subnet_count" {
   description = "Number of subnets"
   type = map(number)
   default = {
-    public = 1,
+    public = 2,
     private = 2
   }
 }
@@ -21,15 +21,15 @@ variable "settings" {
   description = "Configuration settings"
   type = map(any)
   default = {
-    "database" = {
-      allocated_storage = 10
-      db_name           = "roger_db"
-      engine            = "mysql"
-      engine_version    = "8.0"
-      instance_class    = "db.t3.micro"
-      parameter_group_name = "default.mysql8.0"
-      skip_final_snapshot = true
-    },
+    # "database" = {
+    #   allocated_storage = 10
+    #   db_name           = "roger_db"
+    #   engine            = "mysql"
+    #   engine_version    = "8.0"
+    #   instance_class    = "db.t3.micro"
+    #   parameter_group_name = "default.mysql8.0"
+    #   skip_final_snapshot = true
+    # },
     "web_app" = {
       count         = 1
       instance_type = "t2.micro"
@@ -55,14 +55,3 @@ variable "my_ip" {
   sensitive = true
 }
 
-variable "db_username" {
-  description = "Database master user"
-  type = string
-  sensitive = true
-}
-
-variable "db_password" {
-  description = "Database master user password"
-  type = string
-  sensitive = true
-}
